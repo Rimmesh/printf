a48cf6b32afb826c01dc1b9a82fa3e715ca1af0c
Rimmesh <Rimmesh>
2023-04-28 05:59:16 -0700

    commit

--- _printf.c
+++ _printf.c
@@ -4,41 +4,52 @@
 /**
  * _printf - the function printf
  * @format: format
- * Return: the chars printed 
+ * Return: the chars printed
+ *
  */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
+va_list args;
+va_start(args, format);
+int count = 0;
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                char c = (char) va_arg(args, int);
-                putchar(c);
-                count++;
-            } else if (*format == 's') {
-                char *str = va_arg(args, char *);
-                while (*str != '\0') {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            } else if (*format == '%') {
-                putchar('%');
-                count++;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
+putchar(c);
+count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+count++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+count++;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
 
-    va_end(args);
-    return count;
+va_end(args);
+return (count);
 }
 
